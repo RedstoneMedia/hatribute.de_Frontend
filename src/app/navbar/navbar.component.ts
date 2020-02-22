88c04e44be6d24da3cac703c3047a4ac0b8fd97e
RedstoneMedia <justus2881@gmail.com>
2020-02-22 18:23:09 +0100

    Added auto logout (only works in firefox)

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DataService } from '../dataService';
 import { Router } from '@angular/router';
 import { BackendSession } from '../BackendSession';
+import { BackendAboutMe } from '../about-me/BackendAboutMe';
 
 @Component({
   selector: 'app-navbar',
@@ -15,13 +16,40 @@ export class NavbarComponent implements OnInit {
   currentlyLoggedIn: boolean;
   currentRole: number;
   BSession: any;
+  backendAboutMe: BackendAboutMe;
 
   constructor(private data: DataService, private client: HttpClient, protected router: Router) { }
+
+  onBeforeUnload(e) {
+    if (this.currentlyLoggedIn) {
+      this.backendAboutMe.logout(() => {
+        console.log("Logged Out Successfully");
+      }, (error) => {
+        console.error("Could not log out");
+      });
+    }
+    delete e['returnValue'];
+  }
 
   ngOnInit() {
     this.data.curRoute.subscribe(currentRoute => this.currentRoute = currentRoute);
     this.data.currentlyLoggedIn.subscribe(currentlyLoggedIn => this.currentlyLoggedIn = currentlyLoggedIn);
     this.data.currentRole.subscribe(currentRole => this.currentRole = currentRole);
+    this.backendAboutMe = new BackendAboutMe(this.client, this);
+
+    window.addEventListener('beforeunload', (e) => {
+      if (this.currentlyLoggedIn) {
+        this.backendAboutMe.logout(() => {
+          console.log("Logged Out Successfully");
+        }, (error) => {
+          console.error("Could not log out");
+        });
+      }
+      delete e['returnValue'];
+    }, false);
+
+    window.onbeforeunload = this.onBeforeUnload;
+
   }
 
 }
