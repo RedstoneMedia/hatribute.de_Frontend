b1b8eec548a57beb71947fd081b29b5917b1f997
RedstoneMedia <34373974+RedstoneMedia@users.noreply.github.com>
2020-11-05 19:36:11 +0100

    Added static server url and added github to footer

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,6 @@
 
 export class Constants {
-  public static readonly backendUrl = "192.168.178.23";
+  public static readonly backendUrl = "193.26.157.108";
   public static readonly requestProtocol = "http";
   public static readonly adminReadOnlyKeys = ["id", "newToken", "CourseId"];
 }
